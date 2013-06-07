ab48d216c58be9be12bbe59e471c1f33a7daf2d6
Fabian Knittel <fabian.knittel@lettink.de>
2013-06-07 21:02:18 +0200

    Jobs with priority, job queue as priority queue
    In addition, queue processing now takes the priority into account.  Only
    high priority jobs may force two sprinklers active.  Only one normal
    priority job is allowed.

--- daemon.py
+++ daemon.py
@@ -283,6 +283,9 @@ class Queue(object):
     def is_empty(self):
         return len(self._queue) == 0
 
+    def peek(self):
+        return self._queue[0]
+
     def pop(self):
         return self._queue.pop(0)
 
@@ -297,13 +300,30 @@ class Queue(object):
 
 
 class SprinklerJob(object):
-    def __init__(self, job_id, sprinkler_id, duration):
+    def __init__(self, job_id, sprinkler_id, duration, high_priority=False):
         self.job_id = job_id
         self.sprinkler_id = sprinkler_id
         self.duration = duration
+        self.high_priority = high_priority
 
         self.start_time = None
         self.timer = None
+
+
+class MaxActiveSprinklerJobPolicy(object):
+    def __init__(self, max_total, max_low_priority):
+        self._max_total = max_total
+        self._max_low_priority = max_low_priority
+
+    def is_job_runnable(self, job, waiting_jobs, active_jobs):
+        if self._max_total <= len(active_jobs):
+            return False
+        if job.high_priority:
+            return True
+
+        num_jobs_low_priority = len([job for job in active_jobs \
+                if not job.high_priority])
+        return self._max_low_priority > num_jobs_low_priority
 
 
 class JobQueue(object):
@@ -315,6 +335,9 @@ class JobQueue(object):
 
     def is_empty(self):
         return self._queue.is_empty()
+
+    def peek(self):
+        return self._queue.peek()
 
     def pop(self):
         return self._queue.pop()
@@ -329,20 +352,70 @@ class JobQueue(object):
         return self._queue.list_all()
 
 
+class PriorityJobQueue(object):
+    def __init__(self):
+        self._queues = []
+
+    def _get_queue_for(self, job):
+        priority = 1 if job.high_priority else 0
+
+        last_position = 0
+        for queue_priority, queue in self._queues:
+            if queue_priority == priority:
+                return queue
+            if queue_priority < priority:
+                break
+            last_position += 1
+
+        new_queue = JobQueue()
+        self._queues.insert(last_position, (priority, new_queue))
+        return new_queue
+
+    def push(self, job):
+        self._get_queue_for(job).push(job)
+
+    def _get_first_nonempty_queue(self, default=JobQueue()):
+        for _, queue in self._queues:
+            if not queue.is_empty():
+                return queue
+        return default
+
+    def is_empty(self):
+        return self._get_first_nonempty_queue().is_empty()
+
+    def peek(self):
+        return self._get_first_nonempty_queue().peek()
+
+    def pop(self):
+        return self._get_first_nonempty_queue().pop()
+
+    def remove(self, job_id):
+        for _, queue in self._queues:
+            job = queue.remove(lambda x: x.job_id == job_id)
+            if job is not None:
+                return job
+        raise ValueError('job with id {} not found'.format(job_id))
+
+    def list_all(self):
+        return reduce(lambda jobs, queue: jobs + queue[1].list_all(),
+                self._queues, [])
+
+
 class SprinklerJobQueue(object):
-    def __init__(self, clock, sprinkler_ctrl):
+    def __init__(self, clock, sprinkler_ctrl, queue_policy):
         self._clock = clock
         self._sprinkler_ctrl = sprinkler_ctrl
+        self._queue_policy = queue_policy
         self._last_job_id = 0
-        self._waiting_jobs = JobQueue()
+        self._waiting_jobs = PriorityJobQueue()
         self._active_jobs = JobQueue()
 
-    def add(self, sprinkler_id, duration):
+    def add(self, sprinkler_id, duration, high_priority=False):
         if not self._sprinkler_ctrl.is_valid(sprinkler_id):
             raise RuntimeError('Invalid sprinkler id {}'.format(sprinkler_id))
 
         job_id = self._get_next_job_id()
-        job = SprinklerJob(job_id, sprinkler_id, duration)
+        job = SprinklerJob(job_id, sprinkler_id, duration, high_priority)
         self._waiting_jobs.push(job)
         self._attempt_next_job()
         return job_id
@@ -358,9 +431,13 @@ class SprinklerJobQueue(object):
         return self._waiting_jobs.list_all()
 
     def _attempt_next_job(self):
-        while not self._waiting_jobs.is_empty() and \
-                not self._sprinkler_ctrl.check_delay_activation():
-            job = self._waiting_jobs.pop()
+        while not self._waiting_jobs.is_empty():
+            job = self._waiting_jobs.peek()
+            if not self._queue_policy.is_job_runnable(job,
+                    waiting_jobs=self.list_waiting_jobs(),
+                    active_jobs=self.list_active_jobs()):
+                break
+            self._waiting_jobs.pop()
             try:
                 self._sprinkler_ctrl.turn_on(job.sprinkler_id)
             except SprinklerException as e:
@@ -396,7 +473,8 @@ class SprinklerJobQueue(object):
 def main():
     gpio_ctrl = gpio.GpioController()
     sprinkler_ctrl = SprinklerController()
-    sprinkler_job_queue = SprinklerJobQueue(reactor, sprinkler_ctrl)
+    queue_policy = MaxActiveSprinklerJobPolicy(max_total=2, max_low_priority=1)
+    sprinkler_job_queue = SprinklerJobQueue(reactor, sprinkler_ctrl, queue_policy)
 
     load_test_sprinklers(gpio_ctrl, sprinkler_ctrl)
     load_sprinkler_interceptors(sprinkler_ctrl)
