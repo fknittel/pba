2d5f354e754c7d554bab15dc207f14c3c1d92de1
Fabian Knittel <fabian.knittel@lettink.de>
2013-06-30 20:09:11 +0200

    Remove unused spinkler module, add test case for real board

---
+++ test02.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+from time import sleep
+from gpio import *
+import sys
+
+ctrl = GpioController()
+
+GPIO0_BASE = 0 * 32
+GPIO1_BASE = 1 * 32
+GPIO2_BASE = 2 * 32
+GPIO3_BASE = 2 * 32
+
+p_court1 = ctrl.export(GPIO2_BASE + 25)
+p_court2 = ctrl.export(GPIO2_BASE + 24)
+p_court3 = ctrl.export(GPIO2_BASE + 1)
+p_court4 = ctrl.export(GPIO0_BASE + 27)
+p_court5 = ctrl.export(GPIO2_BASE + 23)
+p_court6 = ctrl.export(GPIO2_BASE + 22)
+p_courts = [p_court1, p_court2, p_court3, p_court4, p_court5, p_court6]
+
+for court in p_courts:
+  court.turn_off()
+pcounter = 0
+while True:
+  if pcounter < 6:
+    p_court1.toggle()
+  elif pcounter < 12:
+    p_court2.toggle()
+  elif pcounter < 18:
+    p_court3.toggle()
+  elif pcounter < 24:
+    p_court4.toggle()
+  elif pcounter < 30:
+    p_court5.toggle()
+  elif pcounter < 36:
+    p_court6.toggle()
+  elif pcounter < 42:
+    for court in p_courts:
+      court.toggle()
+  pcounter += 1
+  if pcounter == 42:
+    pcounter = 0
+  sleep(0.5)
