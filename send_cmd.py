a6dfc722c92647b313ac1de4b97dd07314a1c734
Fabian Knittel <fabian.knittel@lettink.de>
2013-07-03 21:05:11 +0000

    send_cmd.py: use urllib2 instead of httplib2

--- send_cmd.py
+++ send_cmd.py
@@ -2,7 +2,7 @@
 # vim:set ts=4 sw=4 et:
 from __future__ import (absolute_import, division, print_function,
         unicode_literals)
-import httplib2
+import urllib2
 import json
 import sys
 
@@ -15,23 +15,33 @@ def add_job(sprinkler_id, duration):
         'duration': duration,
         'high_priority': False
         })
-    h = httplib2.Http()
-    resp, content = h.request(BASE_URL + '/jobs',
-            'POST', jsondata,
-            headers={'Content-Type': 'application/json'})
-    if resp['status'] != '200':
-        raise RuntimeError('request failed', resp, content)
-    job_id = json.loads(content)['job_id']
+    url = BASE_URL + '/jobs'
+    req = urllib2.Request(url, jsondata, {'Content-Type': 'application/json'})
+    f = urllib2.urlopen(req)
+    response = f.read()
+    f.close()
+    job_id = json.loads(response)['job_id']
     print('added job with id {0}'.format(job_id))
 
 
-cmd = sys.argv[1]
-if cmd == 'add-job':
-    sprinkler_id = sys.argv[2]
-    duration = int(sys.argv[3])
-    add_job(sprinkler_id, duration)
-else:
-    print('error: unknown command {0}'.format(cmd), file=sys.stderr)
+def show_usage():
     print('usage: {0} add-job SPRINKLER_ID DURATION'.format(sys.argv[0]),
             file=sys.stderr)
     sys.exit(1)
+
+
+def main():
+    if len(sys.argv) != 4:
+        show_usage()
+    cmd = sys.argv[1]
+    if cmd == 'add-job':
+        sprinkler_id = sys.argv[2]
+        duration = int(sys.argv[3])
+        add_job(sprinkler_id, duration)
+    else:
+        print('error: unknown command {0}'.format(cmd), file=sys.stderr)
+        show_usage()
+
+
+if __name__ == '__main__':
+    main()
