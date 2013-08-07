3cc38e6433189ef192e2ccee2297988840e4280e
Fabian Knittel <fabian.knittel@lettink.de>
2013-08-07 22:32:12 +0200

    backend: provide unified job queue, access to information of single jobs

--- src/pba/client/web.py
+++ src/pba/client/web.py
@@ -15,16 +15,28 @@ def json_response(request, raw_response):
 class JobsResource(resource.Resource):
     def __init__(self, job_queue):
         resource.Resource.__init__(self)
-        self.job_queue = job_queue
+        self._job_queue = job_queue
 
-        self.putChild('active', ActiveJobsResource(self.job_queue))
-        self.putChild('waiting', WaitingJobsResource(self.job_queue))
+        self.putChild('active', ActiveJobsResource(self._job_queue))
+        self.putChild('waiting', WaitingJobsResource(self._job_queue))
 
     def render_POST(self, request):
         new_job = json.loads(request.content.getvalue())
-        job_id = self.job_queue.add(new_job['sprinkler_id'],
+        job_id = self._job_queue.add(new_job['sprinkler_id'],
                 new_job['duration'], new_job['high_priority'])
         return json_response(request, {'job_id': job_id})
+
+    def render_GET(self, request):
+        return json_response(request, [job.for_json() for job \
+                in (self._job_queue.list_active_jobs() + \
+                        self._job_queue.list_waiting_jobs())])
+
+    def getChild(self, path, request): 
+        job_id = int(path)
+        if self._job_queue.is_job_active(job_id):
+            return ActiveJobResource(job_queue=self._job_queue,
+                    job_id=job_id)
+        return WaitingJobResource(job_queue=self._job_queue, job_id=job_id)
 
 
 class CourtsResource(resource.Resource):
@@ -58,6 +70,10 @@ class ActiveJobResource(resource.Resource):
         self._job_queue = job_queue
         self._job_id = job_id
 
+    def render_GET(self, request):
+        job = self._job_queue.get_active_job(self._job_id)
+        return json_response(request, job.for_json())
+
     def render_DELETE(self, request):
         self._job_queue.remove_active_job(self._job_id)
         return b''
@@ -73,7 +89,6 @@ class WaitingJobsResource(resource.Resource):
                 in self._job_queue.list_waiting_jobs()])
 
     def getChild(self, path, request): 
-        print('path ' + repr(path))
         return WaitingJobResource(job_queue=self._job_queue, job_id=int(path))
 
 
@@ -83,8 +98,11 @@ class WaitingJobResource(resource.Resource):
         self._job_queue = job_queue
         self._job_id = job_id
 
+    def render_GET(self, request):
+        job = self._job_queue.get_waiting_job(self._job_id)
+        return json_response(request, job.for_json())
+
     def render_DELETE(self, request):
-        print('job_id ' + repr(self._job_id))
         self._job_queue.remove_waiting_job(self._job_id)
         return b''
 
--- src/pba/core/job_queue.py
+++ src/pba/core/job_queue.py
@@ -32,16 +32,30 @@ class Queue(object):
     def list_all(self):
         return copy.deepcopy(self._queue)
 
+    def find(self, match_func):
+        for _, item in enumerate(self._queue):
+            if match_func(item):
+                return item
+        return None
+
 
 class SprinklerJob(object):
+    JOB_WAITING = 'waiting'
+    JOB_ACTIVE = 'active'
+    JOB_FINISHED = 'finished'
+    JOB_CANCELLED = 'cancelled'
+
     def __init__(self, job_id, sprinkler_id, duration, high_priority=False):
         self.job_id = job_id
         self.sprinkler_id = sprinkler_id
         self.duration = duration
         self.high_priority = high_priority
 
+        self.status = self.JOB_WAITING
         self.start_time = None
+        self.stop_time = None
         self.timer = None
+        self.on_stop = None
 
     def for_json(self):
         return {
@@ -50,7 +64,27 @@ class SprinklerJob(object):
             'duration': self.duration,
             'high_priority': self.high_priority,
             'start_time': self.start_time,
+            'stop_time': self.stop_time,
+            'status': self.status,
         }
+
+    def start(self, clock, on_finished):
+        self.start_time = time.time()
+        self.status = self.JOB_ACTIVE
+        self.timer = defer_later(clock, self.duration,
+                lambda: self._on_finished(on_finished), self)
+
+    def _on_finished(self, on_finished):
+        self.status = self.JOB_FINISHED
+        self.stop_time = time.time()
+        self.on_stop()
+        on_finished()
+
+    def cancel(self):
+        self.status = self.JOB_CANCELLED
+        self.stop_time = time.time()
+        self.timer.cancel()
+        self.on_stop()
 
 
 class MaxActiveSprinklerJobPolicy(object):
@@ -86,13 +120,19 @@ class JobQueue(object):
         return self._queue.pop()
 
     def remove(self, job_id):
-        job = self._queue.remove(lambda x: x.job_id == job_id)
+        job = self._queue.remove(lambda job: job.job_id == job_id)
         if job is None:
             raise ValueError('job with id {} not found'.format(job_id))
         return job
 
     def list_all(self):
         return self._queue.list_all()
+
+    def __contains__(self, job_id):
+        return self.get(job_id) is not None
+
+    def get(self, job_id):
+        return self._queue.find(lambda job: job.job_id == job_id)
 
 
 class PriorityJobQueue(object):
@@ -143,6 +183,17 @@ class PriorityJobQueue(object):
         return reduce(lambda jobs, queue: jobs + queue[1].list_all(),
                 self._queues, [])
 
+    def __contains__(self, job_id):
+        for _, queue in self._queues:
+            if job_id in queue:
+                return True
+        return False
+
+    def get(self, job_id):
+        for _, queue in self._queues:
+            job = queue.get(job_id)
+            if job is not None:
+                return job
 
 class SprinklerJobQueue(object):
     def __init__(self, clock, sprinkler_ctrl, queue_policy):
@@ -162,6 +213,7 @@ class SprinklerJobQueue(object):
 
         job_id = self._get_next_job_id()
         job = SprinklerJob(job_id, sprinkler_id, duration, high_priority)
+        job.on_stop = lambda: self._turn_off(job)
         self._waiting_jobs.push(job)
         self._attempt_next_job()
         return job_id
@@ -191,19 +243,16 @@ class SprinklerJobQueue(object):
                 self._attempt_next_job()
                 continue
 
-            job.start_time = time.time()
-            job.timer = defer_later(self._clock, job.duration,
-                    self._on_end_of_duration, job)
+            job.start(self._clock,
+                    on_finished=lambda: self._on_end_of_duration(job))
             self._active_jobs.push(job)
 
     def remove_active_job(self, job_id):
         job = self._active_jobs.remove(job_id)
-        job.timer.cancel()
-        self._turn_off(job)
+        job.cancel()
 
     def _on_end_of_duration(self, job):
         self._active_jobs.remove(job.job_id)
-        self._turn_off(job)
 
     def _turn_off(self, job):
         try:
@@ -214,3 +263,21 @@ class SprinklerJobQueue(object):
 
     def list_active_jobs(self):
         return self._active_jobs.list_all()
+
+    def is_job_active(self, job_id):
+        return job_id in self._active_jobs
+
+    def is_job_waiting(self, job_id):
+        return job_id in self._waiting_jobs
+
+    def get_waiting_job(self, job_id):
+        return self._waiting_jobs.get(job_id)
+
+    def get_active_job(self, job_id):
+        return self._active_jobs.get(job_id)
+
+    def get_job(self, job_id):
+        job = self.get_active_jobs(job_id)
+        if job is not None:
+            return job
+        return self.get_waiting_jobs(job_id)
