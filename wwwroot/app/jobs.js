a717bab581e9af81796088430498f003850098c5
Michael Nagel <michael.nagel@devzero.de>
2013-08-07 17:43:30 +0200

    jobs: spaces statt tabs

--- wwwroot/app/jobs.js
+++ wwwroot/app/jobs.js
@@ -1,17 +1,17 @@
 function JobsController($scope, $http) {
     $scope.duration = '120';
     $scope.addJob = function(sprinkler_id) {
-    	var new_job = {
-        	sprinkler_id: sprinkler_id,
-        	duration: parseInt($scope.duration),
-    	    high_priority: false,
-    	    };
-	    $http({method: 'POST', url: '/jobs', data: new_job}).
-		    success(function(data, status, headers, config) {
-		    	$scope.refresh();
-	  	    }).
-		    error(function(data, status, headers, config) {
-		    });
+        var new_job = {
+            sprinkler_id: sprinkler_id,
+            duration: parseInt($scope.duration),
+            high_priority: false,
+            };
+        $http({method: 'POST', url: '/jobs', data: new_job}).
+            success(function(data, status, headers, config) {
+                $scope.refresh();
+            }).
+            error(function(data, status, headers, config) {
+            });
     };
 }
 
@@ -19,22 +19,22 @@ function ActiveJobsController($scope, $http) {
     $scope.jobs = [];
  
     $scope.removeJob = function(sprinkler_id) {
-	    $http({method: 'DELETE', url: '/jobs/active/' + sprinkler_id}).
-		    success(function(data, status, headers, config) {
-		    	$scope.refresh();
-	  	    }).
-		    error(function(data, status, headers, config) {
-		    });
+        $http({method: 'DELETE', url: '/jobs/active/' + sprinkler_id}).
+            success(function(data, status, headers, config) {
+                $scope.refresh();
+            }).
+            error(function(data, status, headers, config) {
+            });
     };
  
     $scope.refresh = function() {
-	    $http({method: 'GET', url: '/jobs/active'}).
-		    success(function(data, status, headers, config) {
-		    	$scope.jobs = data;
-	  	    }).
-		    error(function(data, status, headers, config) {
-		    	$scope.jobs = [];
-		    });
+        $http({method: 'GET', url: '/jobs/active'}).
+            success(function(data, status, headers, config) {
+                $scope.jobs = data;
+            }).
+            error(function(data, status, headers, config) {
+                $scope.jobs = [];
+            });
     };
 
     $scope.refresh();
@@ -44,22 +44,22 @@ function WaitingJobsController($scope, $http) {
     $scope.jobs = [];
   
     $scope.removeJob = function(sprinkler_id) {
-	    $http({method: 'DELETE', url: '/jobs/waiting/' + sprinkler_id}).
-		    success(function(data, status, headers, config) {
-		    	$scope.refresh();
-	  	    }).
-		    error(function(data, status, headers, config) {
-		    });
+        $http({method: 'DELETE', url: '/jobs/waiting/' + sprinkler_id}).
+            success(function(data, status, headers, config) {
+                $scope.refresh();
+            }).
+            error(function(data, status, headers, config) {
+            });
     };
 
     $scope.refresh = function() {
-	    $http({method: 'GET', url: '/jobs/waiting'}).
-		    success(function(data, status, headers, config) {
-		    	$scope.jobs = data;
-	  	    }).
-		    error(function(data, status, headers, config) {
-		    	$scope.jobs = [];
-		    });
+        $http({method: 'GET', url: '/jobs/waiting'}).
+            success(function(data, status, headers, config) {
+                $scope.jobs = data;
+            }).
+            error(function(data, status, headers, config) {
+                $scope.jobs = [];
+            });
     };
     
     $scope.refresh();
